8d4ac06cfe5a2268454dabfc77cc4211c38d60f2
ROEL2407 <roel24komen@gmail.com>
2022-03-28 11:20:29 +0200

    Update: old js code added and inndex.js updated

--- index.js
+++ index.js
@@ -1,117 +1,28 @@
-const display = document.getElementById('artItems');
-display.textContent = "Loading...";
+require("dotenv").config();
+const express = require("express");
+const fetch = (...args) =>
+  import("node-fetch").then(({ default: fetch }) => fetch(...args));
 
-const artArray = [];
-const SuggestionArray = [];
-searchOpen();
-itemClick();
-getData();
+const app = express();
+const port = 3000;
 
-/* fetches the data from an api so it can be displayed */
-function getData() {
-    const getURL = 'https://www.rijksmuseum.nl/api/nl/collection?key=OoTZzgc6&ps=100'
-    fetch(getURL)
-        .then(response => response.json())
-        .then(response => {
-            response.artObjects.forEach(art => {
-                artArray.push({
-                    id: art.id,
-                    title: art.title,
-                    maker: art.principalOrFirstMaker,
-                    img: art.webImage.url,
-                    place: art.productionPlaces,
-                    link: art.links.web
-                });
-            })
-            renderData();
-        }).catch(error => console.log(error))
-}
+app.set("view engine", "ejs");
+app.set("views", "./views");
 
-/* renders data in multiple functions */
-function renderData(){
-    display.textContent = "";
-    display.classList.remove("loading");
-    showInfo(artArray);
-    MakeInfobox(artArray);
-    handleRoutes();
+app.use(express.static("public"));
 
-    artArray.forEach(e => {
-        SuggestionArray.push({
-            title: e.title
-        })
+app.get("/", (req, res) => {
+  fetch(`https://www.rijksmuseum.nl/api/nl/collection?key=OoTZzgc6&ps=10`)
+    .then((response) => response.json())
+    .then((data) => {
+      res.render("home", {
+        pageTitle: "Rijksmuseum",
+        data: data.artObjects,
+      });
     })
-    console.log("test" + SuggestionArray);
-}
+    .catch((err) => res.send(err));
+});
 
-function showInfo(data) {
-    data.forEach(item => {
-        const tempItem = document.createElement('a');
-        tempItem.setAttribute("class", "art");
-        tempItem.id = item.id;
-        const output = '<article><div><img src="' + item.img.slice(0, -3) + "=s1000" + '" alt=""></div><h2>' + item.title + '</h2></article>';
-        tempItem.innerHTML = output;
-        display.appendChild(tempItem);
-    })
-}
-
-/* creates an infobox which can be opened to display more information about the art piece */
-function MakeInfobox(data) {
-    data.forEach(item => {
-        const tempInfobox = document.createElement('article');
-        tempInfobox.setAttribute("class", "infobox");
-        tempInfobox.setAttribute("data-route", item.id);
-        tempInfobox.id = "info-box_" + item.id;
-        const output = '<button class="close_popup"><div></div><div></div></button><div><img src="' + item.img.slice(0, -3) + "=s1000" + '" alt=""></div><h2>' + item.title + '</h2><p>' + item.maker + '</p><p>' + item.place + '</p><a href="' + item.link + '" target="_blank">' + item.link + '</a>' ;
-        tempInfobox.innerHTML = output;
-        const infoboxWrapper = document.getElementById("info_boxes");
-        infoboxWrapper.appendChild(tempInfobox);
-    })
-}
-
-function searchOpen() {
-    /* --- get all element for use --- */
-    const search_btn = document.getElementById("search_btn");
-    const search = document.getElementById("search");
-    
-    /* If the button "search" is clicked: */
-    search_btn.addEventListener("click", function (event) {
-        search.classList.toggle("hidden");
-        search_btn.classList.toggle("active");
-    })
-}
-
-
-function itemClick() {
-    let clickId = "";
-    /* checks which item is being clicked and gives the id of the element to the hash router */
-    function onClick(event) {
-        if ( event.target.classList.contains("art")) {
-          clickId = event.target.id;
-        }
-        window.location.hash = clickId;
-    }
-    window.addEventListener('click', onClick);
-}
-
-function updateUI(route) {
-    if (route === "") {
-      const infobox = document.querySelectorAll(".infobox");
-      infobox.forEach(info => {
-        info.classList.remove("active");
-      })
-    } else {
-      /* checks with infobox should be active via the route in the url and gives it the class active */
-      let activeinfoBox = document.querySelector(`[data-route=${route}]`);
-      if (!activeinfoBox.classList.contains("active")) {
-        activeinfoBox.classList.add('active');
-      }
-      const closePopup = document.querySelectorAll(".close_popup");
-      /* removes the class from the item that was active when the user clicks the closing button */
-      closePopup.forEach(function (popUp) {
-        popUp.addEventListener("click", function () {
-          activeinfoBox.classList.remove("active");
-          window.location.hash = "";
-        })
-      })
-    }
-  }
+app.listen(port, () => {
+  console.log(`Now listening on port ${port}`);
+});
