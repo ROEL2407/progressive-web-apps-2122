46f1c54a20924d544576949f086b4e8336dedcc2
ROEL2407 <roel24komen@gmail.com>
2022-04-04 10:56:26 +0200

    Create: route for details page

--- index.js
+++ index.js
@@ -24,11 +24,29 @@ app.get("/", (req, res) => {
     .catch((err) => res.send(err));
 });
 
+
+app.get("/detail/:id", function (req, res) {
+  console.log(req.params.id);
+  fetch(
+    `https://www.rijksmuseum.nl/api/nl/collection/${req.params.id}?key=${apiKey}`
+  )
+    .then(async (response) => {
+      console.log(response);
+      const artWorks = await response.json();
+      res.render("detail", {
+        pageTitle: "Art" + req.params.id,
+        data: artWorks.artObject,
+      });
+    })
+    .catch((err) => res.send(err));
+});
+
 app.get("/offline", (req, res) => {
   res.render("offline", {
     pageTitle: "offline",
   });
 });
+
 
 app.listen(port, () => {
   console.log(`Now listening on port ${port}`);
