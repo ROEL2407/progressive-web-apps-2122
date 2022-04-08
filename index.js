8055fd1b23750ed871853803f22f1903c379ce2c
ROEL2407 <roel24komen@gmail.com>
2022-04-08 12:09:53 +0200

    Update: added code comments where necessary

--- index.js
+++ index.js
@@ -14,6 +14,8 @@ app.set("views", "./views");
 app.use(compression());
 app.use(express.static("public"));
 
+
+// Get all art items with the max of 10 items (performance reasons)
 app.get("/", (req, res) => {
   fetch(`https://www.rijksmuseum.nl/api/nl/collection?key=${apiKey}&ps=10`)
     .then((response) => response.json())
@@ -26,7 +28,7 @@ app.get("/", (req, res) => {
     .catch((err) => res.send(err));
 });
 
-
+// get specific info of 1 art item and display on page
 app.get("/detail/:id", function (req, res) {
   console.log(req.params.id);
   fetch(
@@ -43,6 +45,8 @@ app.get("/detail/:id", function (req, res) {
     .catch((err) => res.send(err));
 });
 
+
+// get offline page if user hasn't cached the page yet
 app.get("/offline", (req, res) => {
   res.render("offline", {
     pageTitle: "offline",
